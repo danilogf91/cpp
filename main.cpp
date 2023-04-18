668ce4681aae454239cc4cbb1a19431819aef73c
danilogf91 <danilogrnd@gmail.com>
2023-04-18 03:37:15 -0500

    Video 37 Operadores basicos not, or y and

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <string.h>
 
 int main ()
 {
 
-  int x{8};
-  int y{8};
-  std::cout << x << ' ' << y << '\n';
-  std::cout << ++x << ' ' << --y << '\n'; // prefijo
-  std::cout << x << ' ' << y << '\n';   
-  std::cout << x++ << ' ' << y-- << '\n'; // postfijo
-  std::cout << x << ' ' << y << '\n';   
-  
+  setlocale (LC_ALL, "es_ES.UTF-8");
+  bool x = false;
+  bool y = false;
+  bool z = true;
+  bool a = false;
+  bool b = false;
+  bool c = true;
+
+  if((x == true || y == true || z == true) && a == true)
+    std::cout << "Hoy no puedo ir a trabajar, estoy enfermo " << '\n';
+  else if ( ( x == true || y == true || z == true ) && b == true )
+    std::cout << "Hoy no puedo ir a trabajar, estoy de mudanza " << '\n';
+  else if ( ( x == true || y == true || z == true ) && c == true )
+    std::cout << "Me ha tocado la quiniela. Dile al jefe que c$@$#/~S " << '\n';
+  else
+    std::cout << "Hoy tengo que ir a trabajar, me he quedado sin disculpas " << '\n';
+      
   return 0;
 }
