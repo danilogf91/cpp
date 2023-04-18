7dc81f798e233ce78eed513b81d6c2383e9edc0c
danilogf91 <danilogrnd@gmail.com>
2023-04-18 02:50:12 -0500

    Video 34 Operadores aritmeticos

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 int main ()
 {
 
-  std::cout << 3 + 2 * 5 << '\n';
-  std::cout << ( 3 + 2 ) * 5;
+  int x{ 7 };
+  int y{ 4 };
+
+  std::cout << "int / int = " << x / y << '\n';
+  std::cout << "double / int = " << static_cast<double>(x) / y << '\n';
+  std::cout << "int / double = " << x / static_cast<double>(y) << '\n';
+  std::cout << "double / double = " << static_cast<double>(x) / static_cast<double>(y) << '\n';
   
   return 0;
 }
