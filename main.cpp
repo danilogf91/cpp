04c9f5a7ba036a21a54cf5d208c01390a9c6cd1e
danilogf91 <danilogrnd@gmail.com>
2023-04-17 23:42:43 -0500

    Video 31 literales

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
+#include <bitset>
 #include <iostream>
-#include <string.h>
 
 int main ()
 {
-  std::cout << "escribe tu nombre y primer apellido: ";
-  std::string nombre{};
-  std::getline(std::cin >> std::ws, nombre);
+  int x { 12 };
+  std::cout << x << '\n';                     // decimal predeterminado
+  std::cout << std::hex << x << '\n';         // hexadecimal
+  std::cout << std::oct << x << '\n';         // octal
+  std::cout << std::dec << x << '\n';         // vuelve a decimal
+  std::cout << x << '\n';                     // decimal  
 
-  std::cout << "Escribe tu edad: ";
-  std::string edad{};
-  std::getline(std::cin >> std::ws, edad);
-  
+  // std::bitset<8> significa que queremos almacenar 8 bits
+  std::bitset<8> bin1{ 0b1100'0101};  // literal binario para binario 1100 0101
+  std::bitset<8> bin2{ 0xC5}; // literal hexadecimal para binario 1100 0101
 
-  std::cout << "Tu nombre es " << nombre << " y tu edad es " << edad << '\n';
+  std::cout << bin1 << ' ' << bin2 << '\n';
+  std::cout << std::bitset<4>{0b1010} << '\n'; // podemos imprimir desde std::bitset directamente
+
+  return 0;
 }
