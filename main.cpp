fb23e244836880939dcaca1f26a8735a9695fad4
danilogf91 <danilogrnd@gmail.com>
2023-04-18 03:01:07 -0500

    Video 35 Modulus(resto) exponenciacion

--- main.cpp
+++ main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
+#include <cstdint> // para std::int_fast64_t
+
+// nota: exp no puede ser negativo
+
+std::int_fast64_t powint (int base, int exp)
+{
+  std::int_fast64_t result{1};
+  while (exp)
+  {
+    if ( exp & 1 )
+      result *= base;
+    exp >>= 1;
+    base *= base;
+  }
+
+  return result;
+
+}
 
 int main ()
 {
 
-  int x{ 7 };
-  int y{ 4 };
+  std::cout << "Escribe un entero: " << '\n';
+  int x{};
+  std::cin >> x;
 
-  std::cout << "int / int = " << x / y << '\n';
-  std::cout << "double / int = " << static_cast<double>(x) / y << '\n';
-  std::cout << "int / double = " << x / static_cast<double>(y) << '\n';
-  std::cout << "double / double = " << static_cast<double>(x) / static_cast<double>(y) << '\n';
-  
+  std::cout << "Escribe otro entero: " << '\n';
+  int y{};
+  std::cin >> y;
+
+  std::cout << "El resto es: " << x % y << '\n';
+
+  if ( ( x % y ) )
+    std::cout << "es uniformemente divisible por  " << y << '\n';
+  else
+    std::cout << "no es uniformemente divisible por  " << y << '\n';
+
+  std::cout << powint (7, 2);
+
   return 0;
 }
